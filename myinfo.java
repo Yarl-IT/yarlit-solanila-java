0d793211dc5a8f79c6481488318193ff201e7dc7
Solanila-Pirapakaran <solanilapirapakaran@gmail.com>
2026-07-16 16:42:14 +0530

    java basic intro

---
+++ myinfo.java
@@ -0,0 +1,11 @@
+class myinfo{
+	public static void main(String args[]){
+		System.out.print("***********\n");
+		System.out.print("myinfo\n");
+		System.out.print("***********\n");
+		System.out.print("First name:pirapakaran\n");
+		System.out.print("Last name:Solanila\n");
+		System.out.print("***********");
+		
+	}
+}
---
+++ myinfo1.java
@@ -0,0 +1,5 @@
+class myinfo1{
+	public static void main(String args[]){
+		System.out.print("******\n myinfo1\n*****\n firstname:pirapakaran\n lastname:solanila\n");
+	}
+}
