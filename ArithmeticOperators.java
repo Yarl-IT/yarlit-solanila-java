191c5862e7b37ce5684ce67d2a907cf3bc4b4740
Solanila-Pirapakaran <solanilapirapakaran@gmail.com>
2026-07-31 09:30:31 +0530

    Create ArithmeticOperators.java

---
+++ ArithmeticOperators.java
@@ -0,0 +1,18 @@
+class ArithmeticOperators{
+	public static void main(String args[]){
+		int x=10;
+		int y=5;
+		
+		System.out.println("x="+x);
+		System.out.println("y="+y);
+		
+		System.out.println("x+y="+(x+y));
+		System.out.println("x-y="+(x-y));
+		System.out.println("x*y="+(x*y));
+		System.out.println("x/y="+(x/y));
+		System.out.println("x%y="+(x%y));
+	}
+}	
+		
+		
+		
