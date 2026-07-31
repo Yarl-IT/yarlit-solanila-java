ef0eae3b34e36dc3541602739e97c8913d79151d
Solanila-Pirapakaran <solanilapirapakaran@gmail.com>
2026-07-31 10:51:40 +0530

    Create Variable3.java

---
+++ Variable3.java
@@ -0,0 +1,13 @@
+class Variable3{
+	public static void main(String args[]){
+		String fname,lname;
+		fname = "Nila";
+		lname = "Pirapa";
+		System.out.println("First name:"+fname);
+		System.out.println("Last name:"+lname);
+		  
+		System.out.println("Full Name:"+fname+" "+lname);
+		  
+	}
+}
+	  
